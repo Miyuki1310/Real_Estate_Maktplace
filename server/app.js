2bd5929d8cbc8359d0936a3fffe7a59419c11750
Khánh Huân <www.khanhhuan@gmail.com>
2024-10-22 18:15:17 +0700

    Create test api route

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { connectDB } from "./db/connectDB.js";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
+
+app.use("/api/user", userRouter);
 
 const start = async () => {
   await connectDB(process.env.DATABASE_URL);
